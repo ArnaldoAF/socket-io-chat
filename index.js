201a512bcd4ee399437bb435a4d83b1a12609508
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-04-08 16:46:46 -0300

    lista online/ver própria mensagem

--- index.js
+++ index.js
@@ -14,27 +14,48 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
+var onlineList = []
+
 io.on('connection', (socket) => {
+    let USER_ID = null
     console.log('a user connected ✨');
 
-    socket.on('chat message', (msg) => {
-    });
+    socket.on('user connected', (userID) => {
+        USER_ID = userID
+        onlineList.push(USER_ID)
+        io.emit('atualizar lista online', onlineList)
+
+    })
 
     socket.on('chat message from front', (msg) => {
         const {user, value} = msg
         const finalMessage = `👤 ${user}: ${value}`
         
         console.log(finalMessage);
-        io.emit('chat message from back', finalMessage);
+        io.emit('chat message from back', msg);
 
       });
     
     socket.on('disconnect', () => {
-        console.log('user disconnected ❌');
+        const finalMessage = ` ${USER_ID} DESCONECTOU! ❌`
+        onlineList = onlineList.filter(id => id != USER_ID)
+        console.log(`finalMessage`);
+        const obj={
+            user: USER_ID,
+            value: finalMessage
+        }
+        io.emit('chat message from back', obj);
+        io.emit('atualizar lista online', onlineList)
+
     });
+
+    // socket.onAny((eventName, ...args) => {// 'hello'
+    //     console.log(onlineList); // [ 1, '2', { 3: '4', 5: ArrayBuffer (1) [ 6 ] } ]
+    //     io.emit('atualizar lista online', onlineList)
+    //   });
 });
 
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
-});
+}); 
