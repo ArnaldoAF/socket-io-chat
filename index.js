cbc938ba4b5fbe8d787fb781ad4dd3d5a5326202
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-04-08 15:32:34 -0300

    chat messages

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
+io.on('connection', (socket) => {
+    console.log('a user connected ✨');
+
+    socket.on('chat message', (msg) => {
+    });
+
+    socket.on('chat message from front', (msg) => {
+        const {user, value} = msg
+        const finalMessage = `👤 ${user}: ${value}`
+        
+        console.log(finalMessage);
+        io.emit('chat message from back', finalMessage);
+
+      });
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected ❌');
+    });
+});
+
+
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+    console.log('server running at http://localhost:3000');
 });
